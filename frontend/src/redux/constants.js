0ff604d3477bc3f7dc2a93584a59b961fc2db1e1
anas baiy <anasbaiy1998@gmail.com>
2025-05-18 18:19:48 +0100

    updated baseurl and built static files

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "mern-azure-app-cgaedrgdg6ftgrha.spaincentral-01.azurewebsites.net";
 export const USERS_URL = '/api/users';
 export const GENRE_URL = '/api/genre';
 export const MOVIE_URL = '/api/movies';
